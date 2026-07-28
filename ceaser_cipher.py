233b058ec2cbe75d20a976875842a34a648ea861
catherine-hendrie <catherine.hendrie@icloud.com>
2026-07-28 00:36:39 -0400

    Added encryption (shift 3)

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,3 +1,18 @@
 #Catherine Hendrie
 #July 27/28 2026
 #Ceaser Cipher
+
+word = input(" ")
+
+encrypt = ""
+
+for letter in word: 
+    upper_letter = letter.upper()
+    number = (ord(upper_letter)) - ord("A")
+    new_number = number+3
+    wrap_num = new_number % 26
+    final_num = wrap_num + ord("A")
+    encrypt_letter = (chr(wrap_num))
+    encrypt = encrypt + encrypt_letter
+
+print(encrypt)
