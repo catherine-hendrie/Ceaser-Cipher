bfdae8f178ae0a715eed015d80671762abce9d8a
catherine-hendrie <catherine.hendrie@icloud.com>
2026-07-30 00:02:41 -0400

    Debugged

--- encrypt_method_experimentation.py
+++ encrypt_method_experimentation.py
@@ -2,7 +2,6 @@
 #July 28 2026
 #Experimentation
 
-import math
 import re
 
 #Getting User Input
@@ -12,17 +11,20 @@ word = input("Please enter your desired encrypted word:")
 parts = re.findall(r"\d+", birthday)
 birthday_num = 0 
 
-for parts in birthday:
-    new_birthday_num = birthday_num + parts 
+for unit in parts:
+    int_unit = int(unit)
+    birthday_num = birthday_num + int_unit
 
 encrypt = ""
+str_new_birthday_num = str(birthday_num)
 
 for letter in word: 
     upper_letter = letter.upper()
     number = (ord(upper_letter)) - ord("A")
-    new_number = number+new_birthday_num
+    new_number = number + birthday_num
+    wrap_num = new_number % 26
     final_num = wrap_num + ord("A")
-    encrypt_letter = (chr(wrap_num))
-    encrypt = encrypt + encrypt_letter +new_birthday_num
+    encrypt_letter = (chr(final_num))
+    encrypt = encrypt + encrypt_letter + str_new_birthday_num
 
 print(encrypt)
