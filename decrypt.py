63940897d5092886ab23867d642b6b568016b472
catherine-hendrie <catherine.hendrie@icloud.com>
2026-08-11 23:48:11 -0400

    Added decryption, small bug fix

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,5 @@
-#Catherine Hendrie
-#July 27/28 2026
-#Ceaser Cipher
+#decrypting
+
 
 word = input(" ")
 
@@ -9,7 +8,7 @@ encrypt = ""
 for letter in word: 
     upper_letter = letter.upper()
     number = (ord(upper_letter)) - ord("A")
-    new_number = number+3
+    new_number = number-3
     wrap_num = new_number % 26
     final_num = wrap_num + ord("A")
     encrypt_letter = chr(final_num)
